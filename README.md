fa843b83fe8ec9501f3f14edc0a12489af7634d3
Andreas Meier <andreas.meier@bbv.ch>
2016-11-27 21:06:43 +0100

    test: add simulation test scripts

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ Setup
 -----
 
 Checkout:
-
 ```
 git clone https://github.com/bbvch/farbsort-websocket.git
 ```
@@ -15,12 +14,30 @@ source venv/bin/activate
 pip install -r requirements.txt
 ```
 
+
+Run
+---
+
 Run server:
 ```
 sudo venv/bin/python server.py
 ```
 
+Run server in simulation mode:
+```
+sudo venv/bin/python server.py --simulate
+```
+
+
+Tests
+-----
+
 Run tests:
 ```
 make tests
+```
+
+Run simulation test script:
+```
+./test-one-stone-at-a-time.sh | sudo venv/bin/python server.py --simulate
 ```
---
+++ test-one-stone-at-a-time.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# conveyor-speed=58mm/s
+
+run_blue_stone () { 
+    echo "lightbarrier1=on"
+    sleep .45
+    echo "lightbarrier1=off"
+    sleep 1.33
+    echo "color=blue"
+    sleep 1.56
+    echo "lightbarrier2=on"
+    sleep .45
+    echo "lightbarrier2=off"
+    sleep .24
+    echo "valve1=on"
+    sleep .3
+    echo "valve1=off"
+}
+
+run_red_stone () { 
+    echo "lightbarrier1=on"
+    sleep .45
+    echo "lightbarrier1=off"
+    sleep 1.33
+    echo "color=red"
+    sleep 1.56
+    echo "lightbarrier2=on"
+    sleep .45
+    echo "lightbarrier2=off"
+    sleep 1.42
+    echo "valve2=on"
+    sleep .3
+    echo "valve2=off"
+}
+
+run_white_stone () {
+    echo "lightbarrier1=on"
+    sleep .45
+    echo "lightbarrier1=off"
+    sleep 1.33
+    echo "color=white"
+    sleep 1.56
+    echo "lightbarrier2=on"
+    sleep .45
+    echo "lightbarrier2=off"
+    sleep 2.19
+    echo "valve3=on"
+    sleep .3
+    echo "valve3=off"
+}
+
+
+sleep 10
+run_blue_stone &
+sleep 10
+run_red_stone &
+sleep 10
+run_white_stone &
+sleep 10
